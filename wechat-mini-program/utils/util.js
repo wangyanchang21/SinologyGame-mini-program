289d2efaff4018ae596033385b46ed53c98c0a55
wangyanchang21 <wangyanchang21@163.com>
2019-09-07 13:18:23 +0800

    接口切换为线上服务器

--- wechat-mini-program/utils/util.js
+++ wechat-mini-program/utils/util.js
@@ -14,7 +14,9 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
-const serverAdress = 'http://localhost:8080/';
+// const serverAdress = 'http://localhost:8080/';
+const serverAdress = 'http://bingying.online:8088/sinology/';
+
 
 module.exports = {
   formatTime: formatTime,
